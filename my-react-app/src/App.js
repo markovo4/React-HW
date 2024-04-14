0a5202152e80ed7483f4ece1fcd56cf8e6daeecc
markovo4 <obf2007@gmail.com>
2024-04-14 16:07:44 +0300

    first commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Profile(){
+  return(
+      <img
+          src="https://i.imgur.com/MK3eW3As.jpg"
+          alt="Katherine Johnson"
+      />
   );
 }
 
-export default App;
+export default Profile;
+
+export function Menu(){
+  return(
+      <div className="container">
+        <nav className="menu">
+          <ul className="menu-list">
+            <li className="menu-list-item">Home</li>
+            <li className="menu-list-item">About</li>
+            <li className="menu-list-item">Contact us</li>
+            <li className="menu-list-item">Products</li>
+          </ul>
+
+        </nav>
+      </div>
+  )
+}
