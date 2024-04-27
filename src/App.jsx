7951113454389affabec90c4d528d98f9d1852c7
markovo4 <obf2007@gmail.com>
2024-04-28 00:11:46 +0300

    hw-React-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Card from './components/Card.jsx';
+
 function App() {
 
     return (
         <>
+            <Card title={'title'}/>
+            <Card text={'text'}/>
+            <Card title={'title'} text={'text'}/>
         </>
     )
 }
