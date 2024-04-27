4a2b4f534859bd9aa4a749f5b6f28e69dfcbeafc
markovo4 <obf2007@gmail.com>
2024-04-28 00:33:32 +0300

    hw-React-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Definitions from './components/Definitions.jsx';
+
 function App() {
+    const definitions = [
+        {dt: 'Coffee', dd: 'Black hot drink', id: 1},
+        {dt: 'Milk', dd: 'White cold drink', id: 2},
+    ];
 
     return (
         <>
+            <Definitions data={definitions}/>
         </>
     )
 }
