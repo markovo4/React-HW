6abcd248d7b20bcf8691e65d8441dfd814f55f79
markovo4 <obf2007@gmail.com>
2024-05-11 15:24:08 +0300

    hw-React-14

--- src/components/Component/Header.jsx
+++ src/components/Component/Header.jsx
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 
-const Header = ({children}) => {
+const Header = ({toggle, children}) => {
+
+    const handleClick = function () {
+        toggle()
+    }
+
     return (
         <div className={'modal-header'}>
             <div className={'modal-title'}>{children}</div>
-            <button type={'button'} className={'btn-close'} data-bs-dismiss={'modal'} aria-label={'Close'}></button>
+            <button type={'button'} className={'btn-close'} data-bs-dismiss={'modal'} aria-label={'Close'}
+                    onClick={handleClick}></button>
         </div>
     )
 }
@@ -13,4 +19,5 @@ export default Header;
 
 Header.propTypes = {
     children: PropTypes.any.isRequired,
+    toggle: PropTypes.func.isRequired
 }
--- src/components/Component/Modal.jsx
+++ src/components/Component/Modal.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Header from "./Header.jsx";
 import Body from "./Body.jsx";
 import Footer from "./Footer.jsx";
+import cn from 'classnames';
 
 export default class Modal extends React.Component {
     static Header = Header;
@@ -14,10 +15,21 @@ export default class Modal extends React.Component {
         super(props);
     }
 
+
     render() {
+        const modalClassName = cn(
+            'modal',
+            this.props.isOpen ? 'fade show' : ''
+        )
+
+        const styleDisplay = cn(
+            this.props.isOpen ? 'block' : 'none'
+        )
+
+
         const {children} = this.props;
         return (
-            <div className={'modal'} style={{display: 'none'}} role={'dialog'}>
+            <div className={modalClassName} style={{display: styleDisplay}} role={'dialog'}>
                 <div className={'modal-dialog'}>
                     <div className={'modal-content'}>{children}</div>
                 </div>
@@ -28,4 +40,5 @@ export default class Modal extends React.Component {
 
 Modal.propTypes = {
     children: PropTypes.any.isRequired,
+    isOpen: PropTypes.bool.isRequired
 }
