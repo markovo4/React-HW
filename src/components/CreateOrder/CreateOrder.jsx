ebc6f98c03d2eb1b5c9c8e9fb062ef70ba95f0b6
markovo4 <obf2007@gmail.com>
2024-07-01 18:15:25 +0300

    react-hw-22

--- src/components/CreateOrder/CreateOrder.jsx
+++ src/components/CreateOrder/CreateOrder.jsx
@@ -3,6 +3,19 @@ import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {fetchProducts} from "../../store/reducers/ActionCreators";
 import styles from './createOrder.module.scss';
+import ClearIcon from '@mui/icons-material/Clear';
+import {
+    Avatar,
+    Box,
+    Container,
+    IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Typography
+} from "@mui/material";
+
 
 const CreateOrder = () => {
     const {products} = useSelector(state => state.listOfProducts)
@@ -18,6 +31,36 @@ const CreateOrder = () => {
 
     return (
         <div className={styles.container}>
+            <Container maxWidth="sm">
+                <Box
+                    sx={{bgcolor: '#cfe8fc'}}
+                    height={'100vh'}
+                >
+                    <Typography sx={{mt: 4, mb: 2}} variant="h6" component="div">
+                        Avatar with text and icon
+                    </Typography>
+                    <List>
+
+                        <ListItem
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="delete">
+                                    <ClearIcon/>
+                                </IconButton>
+                            }
+                        >
+                            <ListItemAvatar>
+                                <Avatar
+                                    src={products.length ? products[0].image : ''}
+                                >
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Single-line item"
+                            />
+                        </ListItem>
+                    </List>
+                </Box>
+            </Container>
             <div className={styles.wrapper}>
                 {products && products.map((item, index) => {
                     return (
